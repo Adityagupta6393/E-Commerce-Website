4f87b1c0be5cdf50105e332096b3677deca1e409
Aditya Gupta <adityagupta6189@gmail.com>
2026-06-05 14:24:44 +0530

    app has done

--- server/app.js
+++ server/app.js
@@ -8,6 +8,22 @@ const connectDB = require('./config/db.js');
 //database connection
 connectDB();
 
+// Middleware
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Routes
+app.use("/api", authRouter);
+app.use("/api/user", usersRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/product", productRouter);
+app.use("/api", brainTreeRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/customize", customizeRouter);
 
 //Run server
 app.listen(process.env.PORT, () => {
